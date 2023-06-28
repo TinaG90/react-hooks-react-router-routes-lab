528b50a09ecc64c95e21fd654e553b34ead39e8d
Altenia Gaines <gaines.tina90@gmail.com>
2023-06-28 15:23:48 -0700

    lab complete

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { actors } from "../data";
 
+const actorsToDisplay = actors.map((actor) => {
+  const actorsMovies = actor.movies.map((movie) => (
+    <li key={movie}>{movie}</li>
+  ));
+  console.log(actorsMovies);
+  return (
+    <div key={actor.name}>
+      <h2>Name: {actor.name}</h2>
+      <p>Movies:</p>
+      <ul>{actorsMovies}</ul>
+    </div>
+  );
+});
+
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorsToDisplay}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const moviesToDisplay = movies.map((movie) => {
+    const genreList = movie.genres.map((genre) => <li key={genre}>{genre}</li>);
+    return (
+      <div key={movie.title}>
+        <h2>Name: {movie.title}</h2>
+        <p>Time: {movie.time}</p>
+        <ul>{genreList}</ul>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {moviesToDisplay}
+    </div>
+  );
 }
 
 export default Movies;
